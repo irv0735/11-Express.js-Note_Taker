a4e2d711b2d551bbffd3ae4199ca67a8cdc5d32a
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-09-05 09:26:18 -0600

    removed const db definition, as it is no longer used

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const db = require('./db/db.json');
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
